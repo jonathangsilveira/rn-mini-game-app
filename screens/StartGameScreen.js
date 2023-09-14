00d645dc3865d3311f299dc6ab13363f40165c89
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-09-14 18:34:19 -0300

    Creating Custom Buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,14 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
 function StartGameScreen() {
-    return <></>
+    return (
+        <View>
+            <TextInput />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    );
 }
 
 export default StartGameScreen;
