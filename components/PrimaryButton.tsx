db69b1ba27038bc445fe48c02fb95740cb750a87
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2023-09-15 18:56:48 -0300

    Adding visual feedback to the buttons

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
 export type PrimaryButtonProps = {
     text: String
@@ -6,10 +6,37 @@ export type PrimaryButtonProps = {
 
 const PrimaryButton = (props: PrimaryButtonProps) => {
     return (
-        <View>
-            <Text>{props.text}</Text>
+        <View style={styles.outerButtonContainer}>
+            <Pressable 
+                style={({pressed}) => 
+                    pressed ? [styles.innerButtonContainer, styles.pressed] : styles.innerButtonContainer
+                } 
+                android_ripple={{color: '#640233'}}>
+                <Text style={styles.buttonText}>{props.text}</Text>
+            </Pressable> 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    innerButtonContainer: {
+        backgroundColor: '#72063C',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    outerButtonContainer: {
+        borderRadius: 24,
+        margin: 4,
+        overflow: 'hidden' // Clip ripple effect
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75 // 75% opace
+    }
+});
 
 export default PrimaryButton;
